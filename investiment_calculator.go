0851bf84942b52eeadd1a40373d82961fa381f58
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-05-24 08:43:55 -0300

    investment-calculator project

--- investiment_calculator.go
+++ investiment_calculator.go
@@ -6,10 +6,24 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
-	var expectedReturnRate = 5.5
-	var years float64 = 10
+	const inflationRate = 6.5 
+	
+	var investmentAmount float64
+	var years float64	
+	var expectedReturnRate float64 
 
-	var futureValue = investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	fmt.Print("Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	fmt.Print("Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
